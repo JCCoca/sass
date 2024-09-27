df9e36b6587a1276bc1f34d3e63b3ab1a4259f95
José Coca <josecoca.cn@gmail.com>
2024-09-26 20:06:58 -0500

    Fix: correção de error de submição multiplas de formulários

--- app/views/layouts/script.php
+++ app/views/layouts/script.php
@@ -4,8 +4,23 @@
 <!-- SB Admin 2 JS -->
 <script src="<?= asset('vendor/sb-admin/js/sb-admin.min.js'); ?>"></script>
 
+<!-- JQuery LoadingOverlay -->
+<script src="<?= asset('vendor/jquery-loading-overlay/loadingoverlay.min.js'); ?>"></script>
+
 <script>
     $(function(){
         $('.money').mask("#.##0,00", {reverse: true});
+
+        $('form').on('submit', (event) => {
+            var form = $(this);
+            form.find('button[type="submit"]').prop("disabled", true);
+
+            $.LoadingOverlay("show", {
+                fontawesome: '',
+                image: '<?= asset('images/loading.svg'); ?>',
+                imageAnimation: false,
+                imageColor: false
+            });
+        });
     });
 </script>

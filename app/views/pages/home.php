bf51131fe680e14e37de809916d4fd7b042c831a
José Coca Carrascosa Neto <jose_coca.cn@hotmail.com>
2024-09-29 16:07:24 -0500

    Feat: home orientador (Não finalizado)

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -1,10 +1,64 @@
 <?php layout('admin/header', ['title' => 'Início', 'active' => 'inicio']); ?>
 
 <?php if (isOrientador()): ?>
+    <div class="row justify-content-center">
+        <div class="col-md-4 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Total de Agendamentos
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">10</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa-regular fa-calendar-lines-pen fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Total de Agendamentos Aprovados
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">10</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa-regular fa-calendar-check fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                Total de Agendamentos Recursados
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">10</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa-regular fa-calendar-xmark fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="card shadow mb-4">
         <div class="card-header py-3">
             <h5 class="card-title text-primary font-weight-bold mb-0">
-                Meus Agendamentos
+                Meus Agendamentos Confirmados
             </h5>
         </div>
         <div class="card-body">
